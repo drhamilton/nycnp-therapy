104a618f2eb198e4aafe9f9d4d630e01ee28c946
dillon <dillonrhamilton@gmail.com>
2023-02-20 21:52:59 -0500

    ad bg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,9 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body className={`${amiri.variable} p-12 font-serif`}>{children}</body>
+      <body className={`${amiri.variable} bg-orange-50 p-12 font-serif`}>
+        {children}
+      </body>
     </html>
   );
 }
